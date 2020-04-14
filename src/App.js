c0335bfc646c580a15a7b93aecbb9e88d49d8469
unknown <bev030997@gmail.com>
2020-04-14 19:44:21 +0300

    add popup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import List from './components/List/List';
+import AddList from './components/AddListButton';
+import DB from './assets/db.json';
 
 function App() {
+
   return (
     <div className="todo">
       <div className="todo__sidebar">
@@ -31,23 +34,12 @@ function App() {
           }
         ]}
           isRemovable />
-        <List items={[
-          {
-            className: "list__add-button",
-            icon: (
-              <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
 
-            ),
-            name: 'Добавить список'
-          }
-        ]} />
-
+        <AddList colors={DB.colors}/>
       </div>
       <div className="todo__tasks"></div>
     </div>
+
   )
 }
 
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import classNames from 'classnames';
+import React from "react";
+import classNames from "classnames";
 
-import './list.scss';
+import '../Badge';
 
-const List = ({items, isRemovable}) => {
+import "./list.scss";
+import Badge from "../Badge";
 
-    return (
-    <ul className="list"> {
-        items.map((item, index) =>    
-        <li key={index} className={classNames(item.className, {'active': item.active})}>
-        <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}</i>
-        <span>{item.name}</span>
-      </li>)
-      }
-
-  </ul>
-    )
-}
+const List = ({ items, isRemovable, onClick }) => {
+  return (
+    <ul onClick={onClick} className="list">
+      {" "}
+      {items.map((item, index) => (
+        <li
+          key={index}
+          className={classNames(item.className, { active: item.active })}
+        >
+          <i>
+            {item.icon ? (
+              item.icon
+            ) : (
+            <Badge color={item.color}/>
+            )}
+          </i>
+          <span>{item.name}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default List;
