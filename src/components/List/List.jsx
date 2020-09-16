7231b178aabfd47d0ee5b85ac620f91619baa2cc
brdnkva <bev030997@gmail.com>
2020-09-16 18:22:12 +0300

    fix colors in List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import classNames from "classnames";
-import "../Badge";
-import "./list.scss";
-import Badge from "../Badge";
+import Badge from "../Badge/Badge";
+
 import removeSvg from "../../assets/img/remove.svg";
+
+import "./List.scss";
 
 const List = ({ items, isRemovable, onClick, onRemove }) => {
   const removeList = (item) => {
@@ -20,7 +21,7 @@ const List = ({ items, isRemovable, onClick, onRemove }) => {
           key={index}
           className={classNames(item.className, { active: item.active })}
         >
-          <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
+          <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
           <span>{item.name}</span>
           {isRemovable && (
             <img
